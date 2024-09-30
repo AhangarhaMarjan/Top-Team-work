17488ca13f11638d46b6e10f18f9512a29e7cad6
mahdiyar.f <mahdiyar.f@akamnet.com>
2024-09-30 20:06:01 +0330

    init authentication config files

--- app.py
+++ app.py
@@ -1,20 +1,71 @@
-app = Flask(__name__)
+from flask import Flask, render_template, redirect, url_for, flash, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from forms import RegistrationForm, LoginForm
+from models import User
+import os
 
-@app.route('/')
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'  # Redirects to login page if not authenticated
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You can now log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route("/home")
+@login_required
 def home():
     return render_template('home.html')
 
-@app.route('/login')
-def login():
-    return "Login Page"
+@app.route("/input")
+@login_required
+def input_page():
+    return "This is the input page, restricted to logged-in users."
 
-@app.route('/register')
-def register():
-    return "Register Page"
-
-@app.route('/input')
-def input_data():
-    return "Input Data Page"
+@app.route("/predict")
+@login_required
+def predict():
+    return "This is the prediction results page, restricted to logged-in users."
 
 if __name__ == '__main__':
     app.run(debug=True)
